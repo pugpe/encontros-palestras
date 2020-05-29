30b8ba0757b7dd060ae8368a12860dc0dbe25310
Anderson Berg <andersonberg@gmail.com>
2020-05-29 13:28:48 -0300

    adicionando spider como script

---
+++ scripts/pugpe.py
@@ -0,0 +1,39 @@
+from scrapy import Spider
+from scrapy.crawler import CrawlerProcess
+
+
+class PugSpider(Spider):
+    name = 'pugpe'
+    allowed_domains = ['pug.pe']
+
+    start_urls = ['http://pycon.pug.pe/archive/past_events/']
+
+    def parse(self, response):
+        event = response.xpath('//h2[@id="home"]/text()').get()
+        talks = response.xpath('//h2[@id="talks"]/following-sibling::div')
+        place = response.xpath(
+            '//h2[@id="local"]/following-sibling::h5').re_first('Local:(.*)')
+        event_time = response.xpath(
+            '//h2[@id="local"]/following-sibling::h5').re_first('Horário:(.*)')
+        for talk in talks:
+            title = talk.xpath('.//h4/text()').get()
+            author = talk.xpath('.//h6/text()').get()
+            abstract = talk.css('p::text').get()
+
+            yield {
+                'titulo': title,
+                'autor': author,
+                'abstract': abstract,
+            }
+
+        if 'archive/past_events/' in response.url:
+            past_urls = response.xpath(
+                '//div[@class="panel"]//a/@href').getall()
+            for next_url in past_urls:
+                yield response.follow(next_url, callback=self.parse)
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(PugSpider)
+    process.start()
