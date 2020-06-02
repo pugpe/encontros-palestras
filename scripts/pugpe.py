3fd188a708c3fd2971d69060cc745d3d87c08b88
Anderson Berg <andersonberg@gmail.com>
2020-06-02 00:07:34 -0300

    adicionando gerador de readme

--- scripts/pugpe.py
+++ scripts/pugpe.py
@@ -3,6 +3,7 @@ from click import argument, command
 from scrapy import Request
 from scrapy import Spider
 from scrapy.crawler import CrawlerProcess
+import readme_generator
 
 
 class PugSpider(Spider):
@@ -10,28 +11,37 @@ class PugSpider(Spider):
     allowed_domains = ['pug.pe']
 
     def parse(self, response):
-        event = response.xpath('//h2[@id="home"]/text()').get()
-        talks = response.xpath('//h2[@id="talks"]/following-sibling::div')
-        place = response.xpath(
-            '//h2[@id="local"]/following-sibling::h5').re_first('Local:(.*)')
-        event_time = response.xpath(
-            '//h2[@id="local"]/following-sibling::h5').re_first('Horário:(.*)')
-        for talk in talks:
-            title = talk.xpath('.//h4/text()').get()
-            author = talk.xpath('.//h6/text()').get()
-            abstract = talk.css('p::text').get()
-
-            yield {
-                'titulo': title,
-                'autor': author,
-                'abstract': abstract,
-            }
-
         if 'archive/past_events/' in response.url:
             past_urls = response.xpath(
                 '//div[@class="panel"]//a/@href').getall()
             for next_url in past_urls:
                 yield response.follow(next_url, callback=self.parse)
+
+        event = response.xpath('//h2[@id="home"]/text()').get()
+        talks = response.xpath('//h2[@id="talks"]/following-sibling::div')
+        place = response.xpath(
+            '//h2[@id="local"]/following-sibling::h5').re_first('Local:(.*)')
+        event_time = response.xpath(
+            '//h2[@id="local"]/following-sibling::h5').re_first('Horário:(.*)</h5>')
+        talks_list = []
+        for talk in talks:
+            title = talk.xpath('.//h4/text()').get()
+            author = talk.xpath('.//h6/text()').get()
+            abstract = talk.css('p::text').get()
+
+            talks_list.append({
+                'titulo': title,
+                'autor': author,
+                'abstract': abstract,
+            })
+
+        if event and talks_list:
+            yield {
+                'evento': event,
+                'local': place,
+                'horario': event_time,
+                'palestras': talks_list,
+            }
 
 
 @command()
@@ -45,13 +55,14 @@ def main(editions):
     process = CrawlerProcess(
         settings={
             "FEEDS": {
-                "items.csv": {"format": "csv"},
+                "temp/talks.json": {"format": "json"},
             },
         }
     )
     process.crawl(PugSpider, start_urls=start_urls)
     process.start()
+    readme_generator.main()
 
 
 if __name__ == '__main__':
-    main()
+    main()  # pylint: disable=no-value-for-parameter
---
+++ scripts/readme_generator.py
@@ -0,0 +1,59 @@
+import os
+import json
+import dateparser
+import re
+
+
+LEVEL1 = "#"
+LEVEL2 = "##"
+LEVEL3 = "###"
+LEVEL4 = "####"
+
+def markdown_generator(event):
+    file_text = []
+    event_date = event["horario"]
+    event_place = event["local"]
+    event_name = event["evento"]
+    event_title = f"{LEVEL1} {event_name} ({event_date})\n"
+    file_text.append(event_title)
+
+    file_text.append(f"Local: {event_place}\n")
+
+    file_text.append(f"{LEVEL2} Palestras\n")
+
+    for talk in event["palestras"]:
+        file_text.append(f"{LEVEL3} {talk['titulo']}")
+        file_text.append(f"{LEVEL4} {talk['autor']}")
+        file_text.append(f"{talk['abstract']}\n")
+
+    parsed_date = dateparser.parse(event_date)
+    dir_name = str(parsed_date.year)
+
+    event_edition_m = re.search(r'^\d+', event_name)
+    if event_edition_m:
+        event_edition = event_edition_m.group()
+
+    write_readme('\n'.join(file_text), dir_name, event_edition)
+
+
+def read_json():
+    with open('temp/talks.json', 'r') as talks_file:
+        return json.load(talks_file)
+
+def write_readme(text, dir_name, event_edition):
+    os.chdir('../')
+    path = f"{dir_name}/{event_edition}"
+    os.makedirs(path, exist_ok=True)
+
+    filename = os.path.join(path, "README.md")
+    with open(filename, 'w') as readme_file:
+        readme_file.writelines(text)
+
+
+def main():
+    data = read_json()
+    for event in data:
+        markdown_generator(event)
+
+if __name__ == '__main__':
+    main()
